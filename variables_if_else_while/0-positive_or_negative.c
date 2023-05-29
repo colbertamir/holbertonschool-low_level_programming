1718c8db36b6bbfb0bd522d512ad455ac7c8f1cb
colbertamir <colbertamir@gmail.com>
2023-05-28 18:27:36 -0700

    Commmit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,9 +12,9 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 1)
+	if (n > 0)
 	{
-		printf("%d is positve\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
