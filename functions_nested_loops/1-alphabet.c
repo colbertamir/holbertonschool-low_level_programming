3f23d97534e3c3dac62bcfe0cbfea9c12b1d5639
colbertamir <colbertamir@gmail.com>
2023-05-30 22:03:49 -0700

    -message

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
-/*
+/**
  * print_alphabet - print all alphabet in lowercase
+ *
+ * Prints alphabet
+ *
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
 {
 	char letter;
-	for (letter ='a'; letter<='z'; letter++)
-		_putchar(letter);
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
 
 	_putchar('\n');
 }
