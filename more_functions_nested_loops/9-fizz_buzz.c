00f6bf6e85634a9afc91ab4eaaa347ee7810f818
colbertamir <colbertamir@gmail.com>
2023-06-04 22:19:36 -0700

    Commit

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,12 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
-	if (i % 3 == 0 && i % 5 != 0)
+	{
+		if (i == 1)
+	{
+		printf("%d", i);
+	}
+	else if (i % 3 == 0 && i % 5 != 0)
 	{
 		printf(" Fizz");
 	}
@@ -24,15 +29,12 @@ int main(void)
 	{
 		printf(" FizzBuzz");
 	}
-	else if (i == 1)
-	{
-		printf("%d", i);
-	}
 	else
 	{
 		printf(" %d", i);
 	}
-}
+	}
+
 	printf("\n");
 
 	return (0);
