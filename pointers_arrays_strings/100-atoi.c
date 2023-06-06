102f2bc7a89a962bb810c25367b60089b56b7262
colbertamir <colbertamir@gmail.com>
2023-06-05 21:48:03 -0700

    REHAB (WINTER IN PARIS) - BRENT FAIYAZ

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,39 +3,44 @@
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
- *
+ * 
  * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	int i, d, n, len;
+	int i, d, n, len, f, digit;
 
 	i = 0;
 	d = 0;
 	n = 0;
-	len = strlen(s);
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	if (s[0] == '-')
-	{
-		d = 1;
-	}
+	while (s[len] != '\0')
+		len++;
 
-	for (i = d; i < len; i++)
+	while (i < len && f == 0)
 	{
-		if (isdigit(s[i]))
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (d % 2)
-			{
 				digit = -digit;
-			}
 			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
-		else
-		{
-			break;
-		}
+		i++;
 	}
+
+	if (f == 0)
+		return (0);
 
 	return (n);
 }
