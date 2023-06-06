11a295caf488f24979eb4b0ea53940de9b58731e
colbertamir <colbertamir@gmail.com>
2023-06-05 21:38:31 -0700

    REHAB (WINTER IN PARIS) - BRENT FAIYAZ

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,45 +1,42 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- *  * _atoi - converts a string to an integer
- *   * @s: string to be converted
- *    *
- *     * Return: the int converted from the string
- *      */
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int i, d, n, len;
 
 	i = 0;
 	d = 0;
 	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+	len = strlen(s);
 
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+	if (s[0] == '-')
 	{
-		if (s[i] == '-')
-			++d;
+		d = 1;
+	}
 
-		if (s[i] >= '0' && s[i] <= '9')
+	for (i = d; i < len; i++)
+	{
+		if (isdigit(s[i]))
 		{
 			digit = s[i] - '0';
 			if (d % 2)
+			{
 				digit = -digit;
+			}
 			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
 		}
-		i++;
+		else
+		{
+			break;
+		}
 	}
-	if (f == 0)
-		return (0);
 
-		return (n);
+	return (n);
 }
