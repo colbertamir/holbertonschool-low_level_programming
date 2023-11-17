f60e12bdd5c9e7b7aa69ce911d3f080df2cdb908
colbertamir <colbertamir1516@gmail.com>
2023-11-17 12:34:53 -0800

    Switched terminals to compile

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,52 +1,29 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_dlistint - Prints every element of dlistint_t list.
- * @h: The head of dlistint_t list.
+ * print_dlistint - prints all elements of a
+ * dlistint_t list
  *
- * Return: The number of nodes in list.
+ * @h: head of the list
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t nodes = 0;
+	int count = 0;
 
-    while (h)
-    {
-        nodes++;
-        printf("%d\n", h->n);
-        h = h->next;
-    }
+	if (h == NULL)
+		return (count);
 
-    return (nodes);
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-    dlistint_t *head;
-    dlistint_t *new;
-    dlistint_t hello = {8, NULL, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        dprintf(2, "Error: Can't malloc\n");
-        return (EXIT_FAILURE);
-    }
-    new->n = 9;
-    head->prev = new;
-    new->next = head;
-    new->prev = NULL;
-    head = new;
-    n = print_dlistint(head);
-    printf("-> %lu elements\n", n);
-    free(new);
-    return (EXIT_SUCCESS);
-}
