b92f6719e44bafde2b6b83923f0ee7d49ea3b827
colbertamir <colbertamir1516@gmail.com>
2023-11-19 21:32:24 -0800

    Complete rewrite

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* Function prototypes */
+hash_node_t *create_new_node(const char *key, const char *value);
+int update_value(hash_node_t *current, const char *value);
+
 /**
  * hash_table_set - Add or update an element in the hash table
  * @ht: Pointer to hash table
@@ -19,38 +23,82 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (update_value(current, value));
+
+		current = current->next;
+	}
+
+	hash_node_t *new_node = create_new_node(key, value);
+
+	if (new_node == NULL)
+		return (0);
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
+
+/**
+ * create_new_node - Creates a new node for the hash table
+ * @key: The key to add
+ * @value: The value associated with the key
+ *
+ * Return: Pointer to the new node created
+ *
+ * Description: Allocates memory for new node, assigns key and value,
+ * and initializes the next pointer to NULL
+ */
+hash_node_t *create_new_node(const char *key, const char *value)
+{
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 
-		if (new_node == NULL)
-		return (0);
+	if (new_node == NULL)
+		return (NULL);
 
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = (value != NULL) ? strdup(value) : NULL;
+	if (value != NULL && new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
+ * update_value - Updates the value of an existing key in the hash table
+ * @current: Pointer to the node whose value is updating
+ * @value: The new value to be updated
+ *
+ * Return: Upon success return 1, otherwise return 0
+ *
+ * Description: Updates the value of an existing key in hash table
+ * by assigning a new value to the corresponding node
+ */
+int update_value(hash_node_t *current, const char *value)
+{
 	char *new_value = (value != NULL) ? strdup(value) : NULL;
 
-	if (value != NULL && new_value == NULL)
-	{
-		free(new_node);
+	if (new_value == NULL)
 		return (0);
-	}
 
-	char *new_key = strdup(key);
-
-	if (new_key == NULL)
-	{
-		free(new_value);
-		free(new_node);
-		return (0);
-	}
-
-	new_node->key = new_key;
-	new_node->value = new_value;
-	new_node->next = NULL;
-
-	if (ht->array[index] != NULL)
-	{
-		new_node->next = ht->array[index];
-	}
-
-	ht->array[index] = new_node;
+	free(current->value);
+	current->value = new_value;
 
 	return (1);
 }
