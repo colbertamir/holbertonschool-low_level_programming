77d854c27b2e97fad32e43edea63ba81ceed8483
colbertamir <colbertamir1516@gmail.com>
2023-11-19 22:27:55 -0800

    >>> print(ht)

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Print key/value pairs of hash table in order
+ * @ht: Pointer to hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx = 0;
+	hash_node_t *node;
+	char *comma = "";
+	int printed = 0;
+
+	if (!ht || !ht->array)
+		return;
+
+	printf("{");
+	while (idx < ht->size)
+	{
+		node = ((ht->array)[idx]);
+		while (node)
+		{
+			if (printed)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			printed = 1;
+			node = node->next;
+		}
+		idx++;
+	}
+	printf("}\n");
+}
+
